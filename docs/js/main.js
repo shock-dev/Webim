293778e507315cdfcbdf0fc68b85fde87ea13a9d
Andrey <shock.work@mail.ru>
2020-11-21 07:15:28 +0300

    build

---
+++ docs/js/main.js
@@ -0,0 +1,42 @@
+$(function(){
+
+    $('.partners-slider').slick({
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 940,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 620,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    })
+
+    $('.questions__item-head').click(function () {
+        $(this).parent().toggleClass('active')
+        $(this).next().slideToggle()
+    })
+
+    $('.burger').click(function () {
+        $('body').addClass('lock')
+        $('.overlay').addClass('active')
+        $('.header__menu').addClass('active')
+    })
+
+    $('.overlay').click(function () {
+        $('body').removeClass('lock')
+        $('.overlay').removeClass('active')
+        $('.header__menu').removeClass('active')
+    })
+
+});
