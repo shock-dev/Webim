b4c9f2773ecfdbe4bdee95829fab0fc2756fd243
Andrey <shock.work@mail.ru>
2020-11-21 06:39:59 +0300

    #A: slider

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,23 @@ $(function(){
     $('.partners-slider').slick({
         arrows: false,
         slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 940,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 620,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     })
 
     $('.questions__item-head').click(function () {
